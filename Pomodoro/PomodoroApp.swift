ee3634a8b869b2407206a1d6c26ecc89c8e40e1f
Dinoboros <meryl.valier@gmail.com>
2025-06-14 15:27:21 +0200

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Méryl VALIER on 14/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
